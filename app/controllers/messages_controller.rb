5e604c4e384700e00e5cd6c524c15d0278699cee
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2020-11-27 18:57:57 +0900

    チャットルームの名の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,10 +12,8 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
-      render :index  
-      @messages = @room.messages.includes(:user)
+      render :index
     end
-
   end
 
 
